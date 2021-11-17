3edeaffd8bf8996d630e297c8db86ab6f068385d
Marcus Noble <github@marcusnoble.co.uk>
2021-11-17 15:02:00 +0000

    Added support for floats and 32-bit ints
    Signed-off-by: Marcus Noble <github@marcusnoble.co.uk>

--- to.go
+++ to.go
@@ -26,6 +26,30 @@ func Int64P(i int64) *int64 {
 	return &i
 }
 
+func Int32(i *int32) int32 {
+	return *i
+}
+
+func Int32P(i int32) *int32 {
+	return &i
+}
+
+func Float64(i *float64) float64 {
+	return *i
+}
+
+func Float64P(i float64) *float64 {
+	return &i
+}
+
+func Float32(i *float32) float32 {
+	return *i
+}
+
+func Float32P(i float32) *float32 {
+	return &i
+}
+
 func String(s *string) string {
 	return *s
 }
